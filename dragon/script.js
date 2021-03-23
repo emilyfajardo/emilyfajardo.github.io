f4e2171faae6e0ae8e39f1e6b80a0801907cf49a
emilyfajardo <70173821+emilyfajardo@users.noreply.github.com>
2021-03-22 22:50:08 -0400

    Add files via upload

--- dragon/script.js
+++ dragon/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3800, npcCollisionRadius: 100, actionDuration:1000, jumpHeight: 350, makeResponsive: true, removeNPCAfterInteracted: 2000});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3800, npcCollisionRadius: 100, actionDuration:1000, jumpHeight: 350, makeResponsive: true, removeNPCAfterInteractedDelay: 2500, npcSpeed: 400});
 });
